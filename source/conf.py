9a143f272b1f494c9d32abe24d60e2fce8bef6ab
rctgamer3 <rctgamer3@gmail.com>
2025-05-11 02:54:18 +0200

    Added sphinx-copybutton

--- source/conf.py
+++ source/conf.py
@@ -70,7 +70,7 @@ author = 'ChrisPVille & RadicalPlants'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.todo', 'sphinx_reredirects']
+extensions = ['sphinx.ext.todo', 'sphinx_reredirects', 'sphinx_copybutton']
 
 templates_path = ['_templates']
 exclude_patterns = []
